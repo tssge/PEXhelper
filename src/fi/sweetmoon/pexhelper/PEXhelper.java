3951989f67748a0d1279c67178b6e248fabda2d1
tssge <tssge93@gmail.com>
2013-04-18 18:35:09 +0300

    Using MondoCommand (https://github.com/crast/MondoCommand) for subcommand handling now.

--- src/fi/sweetmoon/pexhelper/PEXhelper.java
+++ src/fi/sweetmoon/pexhelper/PEXhelper.java
@@ -3,11 +3,16 @@ package fi.sweetmoon.pexhelper;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import mondocommand.MondoCommand;
+import mondocommand.CallInfo;
+import mondocommand.dynamic.Sub;
 import net.milkbowl.vault.permission.Permission;
 
 public class PEXhelper extends JavaPlugin
@@ -15,181 +20,94 @@ public class PEXhelper extends JavaPlugin
 	private static PEXhelper plugin;
 	protected static final Logger logger = Logger.getLogger("PEXhelper");
 	public static Permission perms = null;
-	public static String[] promGrps  = {
+	public static final String[] PROMOTION_GROUPS  = {
 			"Uusi", "Kokenut", "Uskollinen", "Kunnioitettava"
 		};
 	
 	@Override
 	public void onEnable() {
+		plugin = this;
+		
+		// Setup Vault perms, if fails, disable this plugin
 		if (!setupPermissions() ) {
 			logger.severe(String.format("[%s] - Poistettu käytöstä, koska Vault-pluginia ei pystytty tunnistamaan.", getDescription().getName()));
 			getServer().getPluginManager().disablePlugin(this);
 			return;
 		}
-		plugin = this;
+		
+		// We are using MondoCommand for subcommand handling: registering it
+		MondoCommand base = new MondoCommand();
+		base.autoRegisterFrom(this);
+		getCommand("pexhelper").setExecutor(this);
+		getCommand("ph").setExecutor(this);
+		
+	}
+	
+	// /pexhelper timepromote -command
+	@Sub(description="Nostaa pelaajan ontime-rankkia.", minArgs=1, usage="<pelaaja>", permission="pexhelper.timepromote")
+	public void timepromote(CallInfo call) {
+		String player = call.getArg(0);
+		// Need to cast null to object World, null is an ok arg according to javadocs
+		String primaryGroup = perms.getPrimaryGroup((World) null, player);
+		
+		// Increment the group of the player (iterates through possible groups and then adds)
+		for (int i=0; i < PROMOTION_GROUPS.length - 1; i++) {
+			if (primaryGroup.split(" ")[0].equals(PROMOTION_GROUPS[i])) {
+				perms.playerRemoveGroup((World) null, player, primaryGroup);
+				perms.playerAddGroup((World) null, player, String.format("%s %s", PROMOTION_GROUPS[i+1], primaryGroup.split(" ")[1]));
+			}
+		}
+	}
+	
+	// /pexhelper timedemote -command, mostly same stuff as the timepromote one
+	@Sub(description="Laskee pelaajan ontime-rankkia.", minArgs=1, usage="<pelaaja>", permission="pexhelper.timedemote")
+	public void timedemote(CallInfo call){
+		String player = call.getArg(0);
+		String primaryGroup = perms.getPrimaryGroup((World) null, player);
+		
+		// Decrement the group of the player -- iterates the other way compared to timepromote
+		for (int i = PROMOTION_GROUPS.length - 1; i>0; i--) {
+			if (primaryGroup.split(" ")[0].equals(PROMOTION_GROUPS[i])) {
+				perms.playerRemoveGroup((World) null, player, primaryGroup);
+				perms.playerAddGroup((World) null, player, String.format("%s %s", PROMOTION_GROUPS[i-1], primaryGroup.split(" ")[1]));
+			}
+		}
+	}
+	
+	// /pexhelper timegroupset -command
+	@Sub(description="Vaihtaa pelaajan ontime-rankin valittuun rankkiin.", minArgs=2, usage="<pelaaja> <rank (0-3)>", permission="pexhelper.timegroupset")
+	public void timegroupset(CallInfo call) {
+		String player = call.getArg(0);
+		String rank = call.getArg(1);
+		String primaryGroup = perms.getPrimaryGroup((World) null, player);
+		
+		// Try if the string is actually a number, catch the exception if it can't be converted
+	    try { 
+	        Integer.parseInt(rank); 
+	    } catch(NumberFormatException e) {
+	    	
+	    	// If it's string, we get exception and catch it. We treat the arg as string and iterate through groups
+			for (int i=0; i < PROMOTION_GROUPS.length - 1; i++) {
+				if (rank.equalsIgnoreCase(PROMOTION_GROUPS[i])) {
+					perms.playerRemoveGroup((World) null, player, primaryGroup);
+					perms.playerAddGroup((World) null, player, String.format("%s %s", PROMOTION_GROUPS[i], primaryGroup.split(" ")[1]));
+				}
+			}
+			return;
+	    }
+	    
+	    // If it's an int
+	    int rankInt = Integer.parseInt(rank);
+		if (rankInt >= 0 && rankInt <= 3) { // If the number represents a group
+			String primGrp = perms.getPrimaryGroup((World) null, player);
+			perms.playerRemoveGroup((World) null, player, primaryGroup);
+			perms.playerAddGroup((World) null, player, String.format("%s %s", PROMOTION_GROUPS[rankInt], primGrp.split(" ")[1]));
+		}
 	}
 	
 	private boolean setupPermissions() {
 		RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
 		perms = rsp.getProvider();
 		return perms != null;
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
-		if (command.getName().equalsIgnoreCase("pexhelper")) {
-			if (args.length < 2) {
-				return false;
-			}
-			
-			if (args[0].equalsIgnoreCase("timepromote")) { //Check if promoting
-				Player promo = getServer().getPlayer(args[1]);
-					if (promo == null)
-						return false;
-					
-					if (!(sender instanceof Player)) {
-						if(timePromote(promo)) {
-							sender.sendMessage(String.format("Pelaajan %s \"ontime\" arvoa nostettu!", promo.getName()));
-							return true;
-						}
-						return false;
-					}
-					
-					Player pl = (Player) sender;
-					
-					if (perms.has(pl, "pexhelper.timepromote")) {
-						if(timePromote(promo)) {
-							sender.sendMessage(String.format("Pelaajan %s \"ontime\" arvoa nostettu!", promo.getName()));
-							return true;
-						}
-						return false;
-					} else {
-						sender.sendMessage(String.format("%sSinulla ei ole oikeutta tehdä tuota.", ChatColor.RED));
-						return true;
-					}
-					
-			} else if (args[0].equalsIgnoreCase("timedemote")) { //Check if demoting
-				Player demo = getServer().getPlayer(args[1]);
-				if (demo == null)
-					return false;
-				
-				if (!(sender instanceof Player)) {
-					if(timeDemote(demo)) {
-						sender.sendMessage(String.format("Pelaajan %s \"ontime\" arvoa laskettu!", demo.getName()));
-						return true;
-					} else {
-						return false;
-					}
-				}
-				
-				Player pl = (Player) sender;
-				
-				if (perms.has(pl, "pexhelper.timedemote")) {
-					if(timeDemote(demo)) {
-						sender.sendMessage(String.format("Pelaajan %s \"ontime\" arvoa laskettu!", demo.getName()));
-						return true;
-					} else {
-						return false;
-					}
-				} else {
-					sender.sendMessage(String.format("%sSinulla ei ole oikeuksia tehdä tuota.", ChatColor.RED));
-					return true;
-				}
-			} else if (args[0].equalsIgnoreCase("timegroupset")) {
-				Player setPl = getServer().getPlayer(args[1]);
-				if (setPl == null && args[2] == null)
-					return false;
-				
-				if (!(sender instanceof Player)) {
-					    try { 
-					        Integer.parseInt(args[2]); 
-					    } catch(NumberFormatException e) {
-							if (timeGroupSetStr(setPl, args[2])) {
-								sender.sendMessage(String.format("Pelaajan %s \"ontime\" arvo asetettu arvoon %s!", setPl.getName(), args[2]));
-								return true;
-							} else {
-								return false;
-							}
-					    }
-					    if (timeGroupSetInt(setPl, Integer.parseInt(args[2]))) {
-					    	sender.sendMessage(String.format("Pelaajan %s \"ontime\" arvo asetettu arvoon %s!", setPl.getName(), promGrps[Integer.parseInt(args[2])]));
-					    	return true;
-					    } else {
-					    	return false;
-					    }
-					} else if (perms.has((Player) sender, "pexhelper.timegroupset")) {
-					    try { 
-					        Integer.parseInt(args[2]); 
-					    } catch(NumberFormatException e) {
-							if (timeGroupSetStr(setPl, args[2])) {
-								sender.sendMessage(String.format("Pelaajan %s \"ontime\" arvo asetettu arvoon %s!", setPl.getName(), args[2]));
-								return true;
-							} else {
-								return false;
-							}
-					    }
-					    if (timeGroupSetInt(setPl, Integer.parseInt(args[2]))) {
-					    	sender.sendMessage(String.format("Pelaajan %s \"ontime\" arvo asetettu arvoon %s!", setPl.getName(), promGrps[Integer.parseInt(args[2])]));
-					    	return true;
-					    } else {
-					    	return false;
-					    }
-					} else {
-						sender.sendMessage(String.format("%sSinulla ei ole oikeutta tehdä tuota.", ChatColor.RED));
-						return true;
-					}
-				}
-			}
-		return false;
-	}
-	
-	private boolean timePromote(Player pl) {
-		String primGrp = perms.getPrimaryGroup(pl);
-		
-		for (int i=0; i < promGrps.length - 1; i++) {
-			if (primGrp.split(" ")[0].equals(promGrps[i])) {
-				perms.playerRemoveGroup(pl, primGrp);
-				perms.playerAddGroup(pl, String.format("%s %s", promGrps[i+1], primGrp.split(" ")[1]));
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	private boolean timeDemote(Player pl) {
-		String primGrp = perms.getPrimaryGroup(pl);
-		
-		for (int i = promGrps.length - 1; i>0; i--) {
-			if (primGrp.split(" ")[0].equals(promGrps[i])) {
-				perms.playerRemoveGroup(pl, primGrp);
-				perms.playerAddGroup(pl, String.format("%s %s", promGrps[i-1], primGrp.split(" ")[1]));
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	private boolean timeGroupSetInt(Player pl, int grp) {
-		if (grp >= 0 && grp <= 3) {
-			String primGrp = perms.getPrimaryGroup(pl);
-			
-			perms.playerRemoveGroup(pl, primGrp);
-			perms.playerAddGroup(pl, String.format("%s %s", promGrps[grp], primGrp.split(" ")[1]));
-			return true;
-		}
-		return false;
-	}
-	
-	private boolean timeGroupSetStr(Player pl, String grp) {
-		String primGrp = perms.getPrimaryGroup(pl);
-		
-		for (int i=0; i < promGrps.length - 1; i++) {
-			if (grp.equalsIgnoreCase(promGrps[i])) {
-				perms.playerRemoveGroup(pl, primGrp);
-				perms.playerAddGroup(pl, String.format("%s %s", promGrps[i], primGrp.split(" ")[1]));
-				return true;
-			}
-		}		
-		return false;
 	}
 }
